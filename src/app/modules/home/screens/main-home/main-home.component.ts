27425b643ca3561369aff41a947c784d8e28cb24
DuyNguyenvt <nnqduy.spkt@gmail.com>
2020-04-22 01:38:15 +0700

    feat(home): show giphies ang load more

--- src/app/modules/home/screens/main-home/main-home.component.ts
+++ src/app/modules/home/screens/main-home/main-home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { HomeService } from 'src/app/modules/home/services/home.service';
-import { procService } from 'src/utils/ngService';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-main-home',
@@ -9,22 +10,18 @@ import { procService } from 'src/utils/ngService';
   styleUrls: ['./main-home.component.scss'],
 })
 export class MainHomeComponent implements OnInit {
-  // homeState: any;
-  // homeService: any;
-  // private foo:Foo = new Foo();
-  constructor(private homeService: HomeService) {}
+  giphies$: Observable<any>;
+  constructor(private homeService: HomeService) {
+    this.giphies$ = this.homeService.searchGiphies;
+  }
 
   ngOnInit(): void {
-    // fetch(
-    //   'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=m78FKgk6mIvBbw3wR7zXTou9jubyEBme&limit=5'
-    // )
-    //   .then((response) => response.json())
-    //   .then((result) => {
-    //     console.log(result);
-    //   });
+    this.homeService.fetchData({ offset: 0 });
+  }
 
-    console.log(this.homeService.getState());
-    this.homeService.setState({ homeMasterData2: 'test' });
-    console.log(this.homeService.getState());
+  loadGiphy() {
+    this.homeService.fetchData({
+      offset: this.homeService.pagination.value.offset + 20,
+    });
   }
 }
--- src/app/modules/home/services/home.service.ts
+++ src/app/modules/home/services/home.service.ts
@@ -1,33 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Subject, BehaviorSubject, Observable } from 'rxjs';
 
-import { ReactService } from 'src/utils/Service/react.service';
+import * as _ from 'lodash';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HomeService extends ReactService {
-  state: any;
-  constructor() {
-    super();
-    this.state = {
-      homeMasterData: {
-        searchGiphies: {
-          data: {},
-          isLoading: {},
-        },
-        favorGiphies: {
-          data: {},
-        },
-      },
-      giphyMasterData: {},
-    };
+export class HomeService {
+  searchGiphies: BehaviorSubject<any> = new BehaviorSubject([]);
+  pagination: BehaviorSubject<any> = new BehaviorSubject({
+    total_count: 0,
+    count: 0,
+    offset: 0,
+  });
+
+  fetchData(params) {
+    const api = `http://api.giphy.com/v1/gifs/search?q='black+pink'&api_key=m78FKgk6mIvBbw3wR7zXTou9jubyEBme&limit=20&offset=${params.offset}`;
+    fetch(api)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        this.searchGiphies.next([...this.searchGiphies.value, ...result.data]);
+        this.pagination.next(result.pagination);
+      });
   }
 
-  changeState(newState: any) {
-    this.state = newState;
+  getSearchGiphies() {
+    return this.searchGiphies.asObservable();
   }
-
-  // getState() {
-  //   return this.state;
-  // }
 }
